de2549fc8a61be5d9da1b35bae4420defee64db0
marchfirst01 <marchfirst3113@gmail.com>
2023-11-26 04:15:34 +0900

    Feat: 동의 버튼 추가(전체 동의 구현현x)

--- src/screens/Terms/Terms.tsx
+++ src/screens/Terms/Terms.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import { Button } from '@/components/atoms/Button';
+import React, { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
 import styled from 'styled-components/native';
 
 const RadioButtonGroup = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
 
   const handleOptionSelect = (option) => {
     if (selectedOptions.includes(option)) {
@@ -19,108 +21,75 @@ const RadioButtonGroup = () => {
     return selectedOptions.includes(option);
   };
 
+  const checkSelectAll = () => {
+    selectedOptions.sort();
+    if (
+      selectedOptions[0] === contents[0].Option &&
+      selectedOptions[1] === contents[1].Option &&
+      selectedOptions[2] === contents[2].Option &&
+      selectedOptions[3] === contents[3].Option &&
+      selectedOptions[4] === contents[4].Option &&
+      selectedOptions[5] === contents[5].Option
+    ) {
+      setSelectAll(true);
+    } else setSelectAll(false);
+  };
+
+  useEffect(() => {
+    checkSelectAll();
+  }, [selectedOptions]);
+
+  const contents = [
+    { text: '(필수) 서비스 이용약관', Option: 'Option 1' },
+    { text: '(필수) 위치기반 서비스 이용약관', Option: 'Option 2' },
+    { text: '(필수) 개인정보 수집 동의', Option: 'Option 3' },
+    { text: '(필수) 결제서비스 이용약관', Option: 'Option 4' },
+    { text: '(선택) 개인정보 제3자 제공 동의', Option: 'Option 5' },
+    { text: '(선택) 광고 수신 및 마케팅 동의', Option: 'Option 6' },
+  ];
+
   return (
-    <Container>
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option')}
-          selected={isOptionSelected('option')}
-        >
-          <RadioInner selected={isOptionSelected('option')}>
-            {isOptionSelected('option') && <RadioInnerSelected />}
-          </RadioInner>
-          <SelectAllWrapper>
-            <SelectAll>전체 동의</SelectAll>
-            <Description>(선택) 광고 수신을 포함하여 동의합니다</Description>
-          </SelectAllWrapper>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
+    <ScrollView>
+      <Container>
+        <Box>
+          <RadioButton
+            onPress={() => handleOptionSelect('option')}
+            selected={isOptionSelected('option')}
+          >
+            <RadioInner selected={isOptionSelected('option')}>
+              {isOptionSelected('option') && <RadioInnerSelected />}
+            </RadioInner>
+            <SelectAllWrapper>
+              <SelectAll>전체 동의</SelectAll>
+              <Description>(선택) 광고 수신을 포함하여 동의합니다</Description>
+            </SelectAllWrapper>
+          </RadioButton>
+          <Check>확인</Check>
+        </Box>
 
-      <Hr />
+        <Hr />
 
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option1')}
-          selected={isOptionSelected('option1')}
-        >
-          <RadioInner selected={isOptionSelected('option1')}>
-            {isOptionSelected('option1') && <RadioInnerSelected />}
-          </RadioInner>
-          <LabelText>(필수) 서비스 이용약관</LabelText>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
+        {contents.map((option, index) => (
+          <Box>
+            <RadioButton
+              key={index}
+              onPress={() => handleOptionSelect(option.Option)}
+              selected={isOptionSelected(option.Option)}
+            >
+              <RadioInner selected={isOptionSelected(option.Option)}>
+                {isOptionSelected(option.Option) && <RadioInnerSelected />}
+              </RadioInner>
+              <LabelText>{option.text}</LabelText>
+            </RadioButton>
+            <Check>확인</Check>
+          </Box>
+        ))}
 
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option2')}
-          selected={isOptionSelected('option2')}
-        >
-          <RadioInner selected={isOptionSelected('option2')}>
-            {isOptionSelected('option2') && <RadioInnerSelected />}
-          </RadioInner>
-          <LabelText>(필수) 위치기반 서비스 이용약관</LabelText>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
-
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option3')}
-          selected={isOptionSelected('option3')}
-        >
-          <RadioInner selected={isOptionSelected('option3')}>
-            {isOptionSelected('option3') && <RadioInnerSelected />}
-          </RadioInner>
-          <LabelText>(필수) 개인정보 수집 동의</LabelText>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
-
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option4')}
-          selected={isOptionSelected('option4')}
-        >
-          <RadioInner selected={isOptionSelected('option4')}>
-            {isOptionSelected('option4') && <RadioInnerSelected />}
-          </RadioInner>
-          <LabelText>(필수) 결제서비스 이용약관</LabelText>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
-
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option5')}
-          selected={isOptionSelected('option5')}
-        >
-          <RadioInner selected={isOptionSelected('option5')}>
-            {isOptionSelected('option5') && <RadioInnerSelected />}
-          </RadioInner>
-          <LabelText>
-            <SelectOption>(선택) </SelectOption>개인정보 제3자 제공 동의
-          </LabelText>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
-
-      <Box>
-        <RadioButton
-          onPress={() => handleOptionSelect('option6')}
-          selected={isOptionSelected('option6')}
-        >
-          <RadioInner selected={isOptionSelected('option6')}>
-            {isOptionSelected('option6') && <RadioInnerSelected />}
-          </RadioInner>
-          <LabelText>
-            <SelectOption>(선택) </SelectOption>광고 수신 및 마케팅 동의
-          </LabelText>
-        </RadioButton>
-        <Check>확인</Check>
-      </Box>
-    </Container>
+        <ButtonWrapper>
+          <Button children="동의" disabled={!selectAll} />
+        </ButtonWrapper>
+      </Container>
+    </ScrollView>
   );
 };
 
@@ -218,6 +187,10 @@ const Check = styled.Text`
   font-size: 14px;
   line-height: 18px; /* 128.571% */
   letter-spacing: -0.28px;
+`;
+
+const ButtonWrapper = styled.View`
+  margin: 0 auto;
 `;
 
 export default RadioButtonGroup;
