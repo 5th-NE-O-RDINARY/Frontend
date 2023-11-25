3a18d06827eb67ae7f19c0b699e6f3efbf44009f
marchfirst01 <marchfirst3113@gmail.com>
2023-11-26 00:10:47 +0900

    Feat: onBoarding 페이지네이션

--- src/components/OnBoarding/Board.tsx
+++ src/components/OnBoarding/Board.tsx
@@ -1,97 +1,63 @@
 import {
-  FlatList,
-  SafeAreaView,
-  StatusBar,
+  AppRegistry,
+  Dimensions,
+  ScrollView,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
 } from 'react-native';
 import React, { useState } from 'react';
-
-type ItemData = {
-  key: string;
-  title: string;
-  content: string;
-};
-
-const OnBoardingData: ItemData[] = [
-  {
-    key: '1',
-    title: '친구와 약속잡고 지각비 정산',
-    content: `우리의 시간은 금이에요!</br>친구를 초대해 약속방을 만들고 지각비 정산까지 1분!`,
-  },
-  {
-    key: '2',
-    title: '지각 시간에 따라 지각비 차등 정산',
-    content: `10분과 1시간은 달라요!</br>얼마나 지각했는지에 따라 지각비 정산이 가능해요!`,
-  },
-  {
-    key: '3',
-    title: '모임 지각비 정산',
-    content: `한 명 때문에 여러명이 기다린 적 있으신가요?</br>모임 구성원 여러명이서도 참여할 수 있어요!`,
-  },
-  {
-    key: '4',
-    title: '대면 중고거래 위약금 설정',
-    content: `대면 중고거래도 사기가 많아졌어요</br>만남 시간에 지각비를 걸어놓고 안전하게 만나요!`,
-  },
-];
-
-type ItemProps = {
-  item: ItemData;
-  textColor: string;
-};
-
-const Item = ({ item, textColor }: ItemProps) => {
-  const contents = item.content?.split('</br>');
-
-  return (
-    <TouchableOpacity style={[styles.item]}>
-      <View style={styles.img}></View>
-      <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-      <Text style={styles.content}>
-        {contents?.map((content, index) => {
-          return (
-            <Text key={index}>
-              {content}
-              {'\n'}
-            </Text>
-          );
-        })}
-      </Text>
-    </TouchableOpacity>
-  );
-};
+import { OnBoardingData } from '@/constants/onBoardingData';
 
 const Board = () => {
-  const renderItem = ({ item }: { item: ItemData }) => {
-    return <Item item={item} textColor="#4876EF" />;
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleScroll = (event: any) => {
+    const offsetX = event.nativeEvent.contentOffset.x;
+    const currentIndex = Math.floor(offsetX / Dimensions.get('window').width);
+    setCurrentPage(currentIndex);
   };
 
   return (
     <>
-      <FlatList
-        data={OnBoardingData}
-        showsHorizontalScrollIndicator={false}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.key}
+      <ScrollView
         horizontal
-      />
+        pagingEnabled
+        onScroll={handleScroll}
+        showsHorizontalScrollIndicator={false}
+      >
+        {OnBoardingData.map((item, index) => (
+          <View key={index} style={styles.container}>
+            <View style={styles.img}></View>
+            <Text style={styles.title}>{item.title}</Text>
+            <View style={styles.contentWrapper}>
+              <Text style={styles.content}>{item.content1}</Text>
+              <Text style={styles.content}>{item.content2}</Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.pagination}>
+        {OnBoardingData.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.paginationDot,
+              index === currentPage && styles.activeDot,
+            ]}
+          />
+        ))}
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    padding: 32,
-
-    borderWidth: 1,
-    borderColor: 'red',
+    width: Dimensions.get('window').width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
   img: {
     width: 311,
@@ -100,6 +66,7 @@ const styles = StyleSheet.create({
     marginBottom: 61,
   },
   title: {
+    color: '#4876EF',
     fontSize: 16,
     fontWeight: '800',
     textAlign: 'center',
@@ -108,6 +75,27 @@ const styles = StyleSheet.create({
   content: {
     fontSize: 11,
     textAlign: 'center',
+  },
+  contentWrapper: {
+    marginBottom: 37,
+  },
+  pagination: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 16,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paginationDot: {
+    width: 4,
+    height: 4,
+    borderRadius: 5,
+    backgroundColor: '#D9D9D9',
+    marginHorizontal: 3,
+  },
+  activeDot: {
+    backgroundColor: '#4876EF',
   },
 });
 
--- src/screens/OnBoarding/OnBoarding.tsx
+++ src/screens/OnBoarding/OnBoarding.tsx
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Board from '@/components/OnBoarding/Board';
 
 const OnBoarding = () => {
   return (
-    <View>
-      <Board />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Board />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default OnBoarding;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
