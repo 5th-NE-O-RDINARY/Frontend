4ad5945d558b5956221ea1ad7d556868e86f91b0
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-25 15:32:40 +0900

    Fix: React Query Devtools 삭제

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { RecoilRoot } from "recoil";
@@ -15,7 +14,6 @@ export default function App() {
           <StatusBar style="auto" />
         </View>
       </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
