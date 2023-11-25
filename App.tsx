4ce9506c1963f8e5366726815aa17a1389da4b51
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-25 14:44:06 +0900

    Feat: 리코일 세팅

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { RecoilRoot } from "recoil";
 
 export default function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <RecoilRoot>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </RecoilRoot>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
