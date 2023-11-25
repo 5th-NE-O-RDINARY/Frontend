10ecbfad6e8eda2e0861ba24371fa0ceec1e8807
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-25 15:02:02 +0900

    Feat: eslint, prettier 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'never' },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react-refresh/only-export-components': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-children-prop': 'off',
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['off', { ignoreRestSiblings: true }],
+    'react-hooks/exhaustive-deps': 'off',
+  },
+  settings: {
+    'import/external-module-folders': ['.yarn'],
+  },
+};
