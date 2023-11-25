44e8935bc2009ea679ffcb87db63232a9e2765c9
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-25 17:22:42 +0900

    Chore: Text 컴포넌트 수정

---
+++ src/components/atoms/CustomText/index.tsx
@@ -0,0 +1,30 @@
+import { TextType, theme } from "@/styles/theme/theme"
+import { HTMLAttributes } from "react"
+import { Text } from "react-native"
+import styled from "styled-components"
+
+/**
+ * @param as Text 컴포넌트의 종류 : 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div';
+ * @param typo Typo theme 선택
+ * @param color Palette theme 선택
+ */
+
+export interface TextProps extends HTMLAttributes<HTMLSpanElement> {
+  as?: 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div'
+  typo: TextType['typo']
+  color?: TextType['color']
+  children: any
+}
+
+export type TextPropsKey = 'typo' | 'color'
+
+export const CustomText = styled(Text)<{
+  typoKey: TextType['typo']
+  colorKey?: TextType['color']
+}>`
+  white-space: pre-wrap;
+  ${({ typoKey }) => theme.typo[typoKey]};
+  color: ${({ colorKey }) => {
+    return colorKey && theme.palette[colorKey]
+  }};
+`
